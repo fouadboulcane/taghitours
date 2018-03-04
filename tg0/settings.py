d7424a7aabc99e88708fb89c78bf1f534872ce68
fouadlee <fouadlee9@gmail.com>
2018-03-04 22:52:33 +0100

    styles update

--- tg0/settings.py
+++ tg0/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'okow@b099y0g^58-ybz6w$ua4gw=b=^$o_vpk7!b#dz99$fc#@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
